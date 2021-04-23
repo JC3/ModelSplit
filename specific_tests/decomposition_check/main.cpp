4a1930c2d1dc441448a2f7381e70df7fc16d6f76
Jason C <jason.cipriani@gmail.com>
2021-04-22 21:32:28 -0400

    found root of issues
    quaternion constructor params are misnamed in header. will investigate further.

--- specific_tests/decomposition_check/main.cpp
+++ specific_tests/decomposition_check/main.cpp
@@ -42,16 +42,23 @@ static void printhr () {
 }
 
 
-// this isn't built into assimp? well, i couldn't find it, anyways...
-static aiMatrix4x4 recompose (const aiVector3D &scale, const aiVector3D &rotateEuler, const aiVector3D &translate) {
+static aiMatrix4x4 recomposeEuler (const aiVector3D &scale, const aiVector3D &rotateEuler, const aiVector3D &translate) {
 
-    // the api here is weird and inconsistent
+#if 0
+    // the api is somewhat inconsistent here
     aiMatrix4x4 m_scale, m_rotate, m_translate;
     aiMatrix4x4::Scaling(scale, m_scale);
     m_rotate = aiMatrix4x4().FromEulerAnglesXYZ(rotateEuler);
     aiMatrix4x4::Translation(translate, m_translate);
 
     return m_translate * m_rotate * m_scale;
+#else
+    // why doesn't this work...
+    //aiQuaternion rotate(rotateEuler.x, rotateEuler.y, rotateEuler.z);
+    // because the param names are all fucked up
+    aiQuaternion rotate(rotateEuler.y, rotateEuler.z, rotateEuler.x);
+    return aiMatrix4x4(scale, rotate, translate);
+#endif
 
 }
 
@@ -76,11 +83,12 @@ static void checkMatrix (const aiMatrix4x4 &aTb) {
     printf("recomposed == input? %s\n", aTb_recomposed.Equal(aTb) ? "YES" : "NO");
 
     aTb.Decompose(aTb_scale, aTb_rotateEuler, aTb_translate);
-    aTb_recomposed = recompose(aTb_scale, aTb_rotateEuler, aTb_translate);
+    aTb_recomposed = recomposeEuler(aTb_scale, aTb_rotateEuler, aTb_translate);
 
     printhr();
     print(aTb_translate, "input translation (w/ euler angle rotation)");
     print(aTb_rotateEuler * (ai_real)(180.0 / AI_MATH_PI), "input rotation in degrees (w/ euler angle rotation)");
+    //print(aiQuaternion(aTb_rotateEuler.x, aTb_rotateEuler.y, aTb_rotateEuler.z));
     print(aTb_scale, "input scale (w/ euler angle rotation)");
     print(aTb_recomposed, "recomposed (w/ euler angle rotation");
     printf("recomposed == input? %s\n", aTb_recomposed.Equal(aTb) ? "YES" : "NO");
@@ -89,19 +97,49 @@ static void checkMatrix (const aiMatrix4x4 &aTb) {
 }
 
 
+/*static void checkIncidentalWeirdness () {
 
+    aiVector3D r(AI_DEG_TO_RAD(45), AI_DEG_TO_RAD(10), AI_DEG_TO_RAD(5));
+    aiQuaternion q(r.y, r.z, r.x);
+
+    aiMatrix4x4 m1 = aiMatrix4x4().FromEulerAnglesXYZ(r);
+    aiMatrix4x4 m2(aiVector3D(1,1,1), q, aiVector3D(0,0,0));
+    aiMatrix4x4 m3(q.GetMatrix());
+
+    print(r, "r (radians)");
+    print(r * (ai_real)(180.0 / AI_MATH_PI), "r (degrees)");
+    print(q, "quaternion(r.x, r.y, r.z)");
+    print(m1, "FromEulerAnglesXYZ(r)");
+    print(m2, "matrix constructor w/ quaternion q(r.x, r.y, r.z))");
+    print(m3, "GetMatrix from quaternion q(r.x, r.y, r.z)");
+
+}*/
+
+
+//#pragma warning (suppress: 4100)
 int main (int argc, char **argv) {
 
+#if 1
     aiMatrix4x4 aTb;
 
     bool readMatrix = (argc > 1 && !strcmp(argv[1], "-r"));
     if (readMatrix) {
         cin >> aTb;
     } else {
-        aTb[0][0] = aTb[1][3] = -1; // scale = [-1, 1, 1], translate = [0, -1, 0].
-        aTb *= aiMatrix4x4().FromEulerAnglesXYZ(45.0, 23.0, -15.0); // shake it up a little!
+        // construct a matrix from some scale + rotation + translate values.
+        aiVector3D scale(-1, 1, 1);
+        aiQuaternion rotate(AI_DEG_TO_RAD(45), AI_DEG_TO_RAD(23), AI_DEG_TO_RAD(-15)); // shake it up a little!
+        aiVector3D translate(0, 1, 0);
+        printhr();
+        print(translate, "initial translation");
+        print(rotate, "initial rotation");
+        print(scale, "initial scale");
+        aTb = aiMatrix4x4(scale, rotate, translate);
     }
 
     checkMatrix(aTb);
+#else
+    checkIncidentalWeirdness();
+#endif
 
 }
