a93935ccc1b7091ee81a3ea662df6a84a503eb07
Jason C <jason.cipriani@gmail.com>
2021-05-01 12:50:58 -0400

    added hpp files to assimp post-deploy script

--- 3rdparty/postdeploy.sh
+++ 3rdparty/postdeploy.sh
@@ -2,6 +2,7 @@
 
 find assimp -name "*.h" -exec unix2dos {} \;
 find assimp -name "*.hh" -exec unix2dos {} \;
+find assimp -name "*.hpp" -exec unix2dos {} \;
 find assimp -name "*.inl" -exec unix2dos {} \;
 unix2dos assimp/LICENSE assimp/CREDITS
 find assimp -name "*.pdb" -exec bzip2 -kf9v {} \;
