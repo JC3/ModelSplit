657644dcd47fdec67947bb8de343233036bd441e
Jason C <jason.cipriani@gmail.com>
2021-05-06 09:48:37 -0400

    add zip library

--- 3rdparty/postdeploy.sh
+++ 3rdparty/postdeploy.sh
@@ -5,4 +5,9 @@ find assimp -name "*.hh" -exec unix2dos {} \;
 find assimp -name "*.hpp" -exec unix2dos {} \;
 find assimp -name "*.inl" -exec unix2dos {} \;
 unix2dos assimp/LICENSE assimp/CREDITS
+
+find zip -name "*.c" -exec unix2dos {} \;
+find zip -name "*.h" -exec unix2dos {} \;
+unix2dos zip/UNLICENSE
+
 find assimp -name "*.pdb" -exec bzip2 -kf9v {} \;
